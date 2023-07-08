8476f030a1f3562757b6aa3694d25dc120b6cba8
Maxim Tsirlin <maximtsirlin@gmail.com>
2023-07-09 02:26:38 +0800

    cube rotation

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,4 +1,6 @@
+import { render } from 'react-dom';
 import * as THREE from 'three';
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js';
 
 const renderer = new THREE.WebGLRenderer();
 
@@ -17,9 +19,30 @@ const camera = new THREE.PerspectiveCamera(
 
 );
 
+const orbit = new OrbitControls(camera, renderer.domElement );
+
 const axesHelper = new THREE.AxesHelper(5);
 scene.add(axesHelper);
 
-camera.position.z = 5;
+// camera.position.z = 5;
+// camera.position.y = 2;
 
-renderer.render(scene, camera);
+camera.position.set(0, 2, 5);
+
+orbit.update();
+
+const boxGeometry = new THREE.BoxGeometry();
+const boxMaterial = new THREE.MeshBasicMaterial({color: 0x00ff00});
+const box = new THREE.Mesh(boxGeometry, boxMaterial);
+scene.add(box);
+
+
+
+function animate(time) {
+    box.rotation.x = time / 1000;
+    box.rotation.y = time / 1000;
+    renderer.render(scene, camera);
+}
+
+renderer.setAnimationLoop(animate);
+
