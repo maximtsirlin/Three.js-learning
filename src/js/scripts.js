5edb7e7214c9db594c539dda37768b9dcc536eba
Maxim Tsirlin <maximtsirlin@gmail.com>
2023-07-14 00:11:11 +0800

    test

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,6 +1,7 @@
 import { render } from 'react-dom';
 import * as THREE from 'three';
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js';
+import * as dat from 'dat.gui'
 
 const renderer = new THREE.WebGLRenderer();
 
@@ -16,7 +17,6 @@ const camera = new THREE.PerspectiveCamera(
     window.innerWidth / window.innerHeight,
     0.1, 
     1000
-
 );
 
 const orbit = new OrbitControls(camera, renderer.domElement );
@@ -24,22 +24,21 @@ const orbit = new OrbitControls(camera, renderer.domElement );
 const axesHelper = new THREE.AxesHelper(5);
 scene.add(axesHelper);
 
-// camera.position.z = 5;
-// camera.position.y = 2;
+
 
 camera.position.set(-10, 30, 30);
 
 orbit.update();
 
-const boxGeometry = new THREE.BoxGeometry();
-const boxMaterial = new THREE.MeshBasicMaterial({
-    color: 0x00ff00
-});
-const box = new THREE.Mesh(boxGeometry, boxMaterial);
-scene.add(box);
+// const boxGeometry = new THREE.BoxGeometry();
+// const boxMaterial = new THREE.MeshBasicMaterial({
+//     color: 0x00ff00
+// });
+// const box = new THREE.Mesh(boxGeometry, boxMaterial);
+// scene.add(box);
 
 const planeGeometry = new THREE.PlaneGeometry(30, 30);
-const planeMaterial = new THREE.MeshBasicMaterial({
+const planeMaterial = new THREE.MeshStandardMaterial({
     color: 0xFFFFFF,
     side: THREE.DoubleSide,
 
@@ -51,16 +50,47 @@ plane.rotation.x = -0.5 * Math.PI;
 const gridHelper = new THREE.GridHelper(30, 100);
 scene.add(gridHelper);
 
-const sphereGeometry = new THREE.SphereGeometry(4);
-const sphereMaterial = new THREE.MeshBasicMaterial({
+const sphereGeometry = new THREE.SphereGeometry(4, 50, 10);
+const sphereMaterial = new THREE.MeshStandardMaterial({
     color: 0x0000FF,
-    wireframe: true});
+    wireframe: false});
 const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
 scene.add(sphere);
+sphere.position.set(0, 10, 0)
+
+const ambientLight = new THREE.AmbientLight(0x333333);
+scene.add(ambientLight);
+
+const directionalLight = new THREE.DirectionalLight(0xFFFFFF, 0.8);
+scene.add(directionalLight);
+
+const gui = new dat.GUI();
+
+const options = {
+    sphereColor: '#ffea00',
+    wireframe: false,
+    speed: 0.01
+};
+
+gui.addColor(options, 'sphereColor').onChange(function(e){
+    sphere.material.color.set(e);
+});
+
+gui.add(options, 'wireframe').onChange(function(e){
+    sphere.material.wireframe = e;
+});
+
+gui.add(options, 'speed', 0, 0.1);
+
+let step = 0;
 
 function animate(time) {
-    box.rotation.x = time / 1000;
-    box.rotation.y = time / 1000;
+    // box.rotation.x = time / 1000;
+    // box.rotation.y = time / 1000;
+
+    step += options.speed;
+    sphere.position.y = 10 * Math.abs(Math.sin(step));
+
     renderer.render(scene, camera);
 }
 
